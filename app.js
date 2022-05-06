a09a523ce54127a8ca4930c3afadfc47a2a012a8
Porameht <porameht@outlook.com>
2022-05-06 22:22:03 +0700

    the first page

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const chalk = require("chalk");
+const port = 3000;
+const debug = require('debug')('app');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+
+// app.use(morgan('combined'));
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname,'/public/')));
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Frank");
+});
+
+app.listen(port, () => {
+  // console.log(`Server port ${chalk.green(port)} is running...`);
+  debug(`Server port ${chalk.green(port)} is running...`);
+});
